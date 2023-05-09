7e04bc177b68aba5062e53d3b4f3c535022e4081
mfelipegs <matheusg.dasilva7@gmail.com>
2023-05-08 23:42:24 -0300

    Chore: Update and Read functions

--- app.py
+++ app.py
@@ -53,5 +53,37 @@ def salvar():
         MessageBox.showinfo("Mensagem", "Cadastro realizado com sucesso.")
         conectar.close()
 
-btn_salvar = Button(tela, text="Salvar", command="salvar", font=("Arial 12")).place(x=100,y=190)        
+def atualizar():
+    id = txt_codigo.get()
+    name = txt_nome.get()
+    phone = txt_telefone.get()
+    if name == "" or phone == "":
+        MessageBox.showinfo("ALERT", "Digite todos os campos para realizar alteração")
+    else:
+        conectar = mysql.connect(host="localhost", user="root", password="", database="Exemplo")
+        cursor = conectar.cursor()
+        cursor.execute("UPDATE teste SET nome= '"+ txt_nome.get() + "', telefone='"+ txt_telefone.get() + "' WHERE codigo = '" + txt_codigo.get() + "'")
+        cursor.execute("commit")
+        MessageBox.showinfo("Status", "Successfully Updated")
+        conectar.close()
+
+def Select():
+    if(txt_codigo.get() == ""):
+        MessageBox.showinfo("ALERT", "Por Favor, digite o código")
+    else:
+        conectar = mysql.connect(host="localhost", user="root", password="", database="Exemplo")
+        cursor = conectar.cursor()
+        cursor.execute("SELECT * FROM teste WHERE codigo= '" + txt_codigo.get() + "'")
+        rows = cursor.fetchall()
+
+        for row in rows:
+            txt_nome.insert(0, row[1])
+            txt_telefone.insert(0, row[2])
+
+        conectar.close()
+
+btn_salvar = Button(tela, text="Salvar", command=salvar, font=("Arial 12")).place(x=100,y=190)     
+btn_excluir = Button(tela, text="Apagar", command=excluir, font=("Arial 12")).place(x=200,y=190)        
+btn_update = Button(tela, text="Update", command=atualizar, font=("Arial 12")).place(x=320,y=190)
+btn_consultar = Button(tela, text="Consultador", command=Select, font=("Arial 12")).place(x=200,y=240)
 tela.mainloop()
